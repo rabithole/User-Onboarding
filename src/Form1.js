35977d50e3032df86c2e6a445a0a3032e605e683
Robert Kulp <pedalinon@gmail.com>
2019-09-13 14:41:36 -0700

    Validation works

--- src/Form1.js
+++ src/Form1.js
@@ -7,13 +7,16 @@ const Form1 = (values) => {
 	return (
 		<Form className='form'>
 			<h4>Add Your Information Below!</h4>
-
+			{values.errors.name && <p className='errors'>{values.errors.name}</p>}
 			<Field type='text' name='name' placeholder='name' className='field'/>
 
+			{values.errors.email && <p className='errors'>{values.errors.email}</p>}
 			<Field type='text' name='email' placeholder='email' className='field'/>
 
+			{values.errors.password && <p className='errors'>{values.errors.password}</p>}
 			<Field type='password' name='password' placeholder='password' className='field'/>
 
+			{values.errors.terms && <p className='errors'>{values.errors.terms}</p>}
 			<Field type='checkbox' name='terms' className='field'/>
 
 			<button type='submit'>Submit</button>
@@ -24,14 +27,22 @@ const Form1 = (values) => {
 export default withFormik({
 	mapPropsToValues: (values) => {
 		return {
+			// This makes the inputs controlled. 
 			name: values.name || '',
 			email: values.email || '',
 			password: values.password || '',
-			
+			terms: values.terms || false
 		}
 	},
 
+	handleSubmit: (values) => {
+		console.log(values);
+	},
+
 	validationSchema: yup.object().shape({
-		name: yup.string().required('Full name is required!')
-	})
+		name: yup.string().required('Full name is required!'), 
+		email: yup.string().email().required('We need an email'),
+		terms: yup.boolean().oneOf([true], 'must read terms'),
+		password: yup.string().required('password required!')
+	}),
 })(Form1)
